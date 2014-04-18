4dd35849de4446a3fe916b7a3a2cbc53e82fc635
unknown <PNP@abc.(none)>
2014-04-18 06:38:28 -0700

    saves all downloaded stocks to mysql table with ticker symbols.

--- AllStocksDownload.R
+++ AllStocksDownload.R
@@ -1,57 +1,61 @@
-#install.packages("quantmod")
-library("quantmod")
-#Script to download prices from yahoo
-#and Save the prices to a RData file
-#The tickers will be loaded from a csv file
+library(TTR)
+library(quantmod)
+library(RMySQL)
+# Download all stocks for NASDAQ
 
-#Script Parameters
-tickerlist <- "companylist.csv"  #CSV containing tickers on rows
-savefilename <- "stockdata.RData" #The file to save the data in
-startDate = as.Date("2005-01-13") #Specify what date to get the prices from
-maxretryattempts <- 5 #If there is an error downloading a price how many times to retry
+cat("Fetching all tickers\n")
 
-#Load the list of ticker symbols from a csv, each row contains a ticker
+all.symbols <- stockSymbols(c("NASDAQ"),sort.by=c("MarketCap","Symbol"),quiet=TRUE)
+# Get 10 biggest by market cap
 
-stocksLst <- read.csv("companylist.csv", header = F, stringsAsFactors = F)
-stockData <- new.env() #Make a new environment for quantmod to store data in
-nrstocks = length(stocksLst[,1]) #The number of stocks to download
+biggest.symbols <- tail(all.symbols,n=10)
 
-#Download all the stock data
-for (i in 1:nrstocks){
-  for(t in 1:maxretryattempts){
-    
-    tryCatch(
-{
-  #This is the statement to Try
-  #Check to see if the variables exists
-  #NEAT TRICK ON HOW TO TURN A STRING INTO A VARIABLE
-  #SEE  http://www.r-bloggers.com/converting-a-string-to-a-variable-name-on-the-fly-and-vice-versa-in-r/
-  if(!is.null(eval(parse(text=paste("stockData$",stocksLst[i,1],sep=""))))){
-    #The variable exists so dont need to download data for this stock
-    #So lets break out of the retry loop and process the next stock
-    #cat("No need to retry")
-    break
-  }
+# Cleanup
+
+clean.symbols <- subset(biggest.symbols, select = c("Symbol","Name","LastSale"))
+#head(all.symbols)
+#head(clean.symbols)
+#clean.symbols
+row.names(clean.symbols) <- NULL
+
+# Download data
+
+cat("Starting download for ",nrow(biggest.symbols)," tickers \n")
+
+for (i in 1:nrow(clean.symbols)){
+  symbol <- clean.symbols[i,]$Symbol
+  cat("Downloading",symbol)
   
-  #The stock wasnt previously downloaded so lets attempt to download it
-  cat("(",i,"/",nrstocks,") ","Downloading ", stocksLst[i,1] , "\t\t Attempt: ", t , "/", maxretryattempts,"\n")
-  getSymbols(stocksLst[i,1], env = stockData, src = "yahoo", from = startDate)
-}
-#Specify the catch function, and the finally function
-, error = function(e) print(e))
+  try (getSymbols(symbol, verbose=FALSE, warnings=FALSE, from='2005-01-01'))
+  
+  if(!exists(symbol)){
+    # print(symbol)
+    cat(", error - did not download (likely due to rate limiting\n")
+    #    clean.symbols <- clean.symbols$symbol <- NULL
+    # print(clean.symbols)
+    #print(clean.symbols$symbol)
   }
+  else {
+    cat("\n")
+  }
+#  Sys.sleep(2)
 }
+# Save data to MySQL
 
 
-#Lets save the stock data to mysql.
-#Connection to mysql.
-con <- dbConnect(MySQL(),user="root", password="root", dbname="stockdata", host="localhost")
-for (tick in nrstocks) {
-  print(tick)
-  df <- get(stocksLst[tick,1], pos=stockData)  # get data from stockData environment
-  df1=setNames(df,c("Open","High","Low","Close","Volume","Adjusted"))
-  #dbwriteTable(con, name="stocknew",value=as.data.frame(df), field.types=list(dte="date", open="double(20,10)",high="double(20,10)",low="double(20,10)",Close="double(20,10)",volume="double(20,10)",Adjusted="double(20,10)"),row.names=FALSE,append=TRUE)
-  print(head(df1))
-  dbWriteTable(con, name='stocksnew', value=as.data.frame(df1),append=TRUE,row.names=FALSE)  
- 
+
+  con <- dbConnect(MySQL(),user="root", password="root", dbname="stock", host="localhost")
+
+
+
+# Loop through each symbol
+
+for (i in 1:nrow(clean.symbols)){
+  symbol <- clean.symbols[i,]$Symbol
+  df = data.frame(symbol,Date=index(get(symbol)), coredata(get(symbol),""))
+  df1=setNames(df,c("symbol","Date","Open","High","Low","Close","Volume","Adjusted"))
+  cat("Storing",symbol,"\n")
+  
+  dbWriteTable(con, name='allstocks', value=as.data.frame(df1), row.names=FALSE,append=TRUE)
+  
 }
