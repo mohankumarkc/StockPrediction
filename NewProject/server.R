f795d3c06bc0560a541f61328fcb31cec5214d23
unknown <Mohan@mohan-pc.(none)>
2014-04-21 16:58:44 +0530

    Label changes.

--- NewProject/server.R
+++ NewProject/server.R
@@ -11,17 +11,34 @@ shinyServer(function(input, output) {
   
   result1 <- dbSendQuery(con, "select distinct(symbol) from allstocks;")
   data <- fetch(result1, n=-1)
-  print(as.list(data))
-  #stocksymbols <- fetch(result1, n = -1)
-  
-  #output$table <-renderTable({result1()})
+  #print(as.list(data))
   
   output$distStock <- renderUI({
     
     selectInput(inputId = "StockSymbols",
-                label = "Stock Symbols",
+                label = "Please Select",
                 choices = data$symbol,
                 selected = "CSCO")
   })
   
+  # Create an environment for storing data
+  
+  
+  # Make a chart for a symbol, with the settings from the inputs
+  make_chart <- function(data) {
+    # symbol_data <- data
+    
+    
+    chartSeries(lsStockSymbol,
+                # name      = data,
+                type      = input$chart_type,
+                subset    = paste("last", input$time_num, input$time_unit),
+                log.scale = input$log_y,
+                theme     = "white")
+  }
+  
+  output$plot_employee1 <- renderPlot({ make_chart("select_employee1") })
+  
+  
 })
+  
--- NewProject/ui.R
+++ NewProject/ui.R
@@ -2,23 +2,16 @@
 # Define UI for dataset viewer application
 shinyUI(pageWithSidebar(
   # Application title
-  headerPanel("Welcome lazy student! You're currently using the program
-              'What Nicolas's teacher wants' "),
+  headerPanel("Stock Prediction"),
   
   
   # Left hand side panel
   sidebarPanel(
     wellPanel(
-      p(strong("Stock Prediction")),
+      p(strong("Stock Symbols")),
       uiOutput("distStock")
       
     )),
-  
-  #         submitButton("Update View"),
-  #        br(),
-  # Button to import data
-  # Simple texts
-  
   
   # Main panel (on the right hand side)
   
@@ -28,10 +21,6 @@ shinyUI(pageWithSidebar(
     tableOutput("table")
   )
   
-  # Numerical summary of the dataset,
-  # coming from the function output$summary in server.R
-  
-  # Graphic
-  # coming from the function output$boxplots in server.R
   ))
 
+  
