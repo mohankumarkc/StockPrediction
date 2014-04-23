69d6e8f234bdf3bf47520fb4612d9f951969e304
unknown <Mohan@mohan-pc.(none)>
2014-04-23 16:41:03 +0530

    Added code which retrive data from db table and plotting for that data - for the selected stock symbol.

--- NewProject/server.R
+++ NewProject/server.R
@@ -1,44 +1,42 @@
 library(shiny)
 library(RMySQL)
+library(quantmod)
+library(xts)
+#To get the connection. Change database name accordingly.
 con <- dbConnect(MySQL(),user="root", password="root", dbname="stockdata", host="localhost")
 
 shinyServer(function(input, output) {
-  # query1<-reactive({"select distinct(symbol) from allstocks"})
-  
-  #    username <- input$uname
-  
-  #result1 <- reactive({dbGetQuery(con,query1())})
-  
+  #Selecting available stock symbols from the db table.  
   result1 <- dbSendQuery(con, "select distinct(symbol) from allstocks;")
   data <- fetch(result1, n=-1)
-  #print(as.list(data))
-  
+
+  #showing all the stock symbols in the dropdown.
   output$distStock <- renderUI({
-    
+      
     selectInput(inputId = "StockSymbols",
                 label = "Please Select",
                 choices = data$symbol,
                 selected = "CSCO")
   })
   
-  # Create an environment for storing data
-  
-  
-  # Make a chart for a symbol, with the settings from the inputs
-  make_chart <- function(data) {
-    # symbol_data <- data
-    
-    
-    chartSeries(lsStockSymbol,
-                # name      = data,
-                type      = input$chart_type,
-                subset    = paste("last", input$time_num, input$time_unit),
-                log.scale = input$log_y,
-                theme     = "white")
-  }
-  
-  output$plot_employee1 <- renderPlot({ make_chart("select_employee1") })
-  
+output$plotSymbol <- renderPlot({
+  #showSelectedSymbol <- input$StockSymbols
+  #print(showSelectedSymbol)
+
+  #Query to retrieve the data for the selected stock symbol from the dropdown. 
+  query1 <- reactive({paste("SELECT Date, Open, High, Low, Close, Volume FROM allstocks WHERE symbol='",input$StockSymbols,"';",sep="")})
+  result2 <- reactive({dbGetQuery(con,query1())})
+  print(head(result2()))
+  PDS <- xts(result2()[,-1],order.by=as.POSIXct(result2()[,1]))
+  print(head(PDS))
+
+
+  chartSeries(PDS, name=input$StockSymbols, theme = "white", 
+              type = input$chart_type, 
+              subset    = paste("last", input$time_num, input$time_unit),              
+              log.scale = FALSE
+              )
+})
+
   
 })
-  
--- NewProject/ui.R
+++ NewProject/ui.R
@@ -1,5 +1,3 @@
-# Set the working directory and list all csv files inside
-# Define UI for dataset viewer application
 shinyUI(pageWithSidebar(
   # Application title
   headerPanel("Stock Prediction"),
@@ -11,16 +9,35 @@ shinyUI(pageWithSidebar(
       p(strong("Stock Symbols")),
       uiOutput("distStock")
       
-    )),
+     ),
+ 
+  
+  selectInput(inputId = "chart_type",
+              label = "Chart Type",
+              choices = c("Candlestick" = "candlesticks",
+                          "Matchstick" = "matchsticks",
+                          "Bar" = "bars",
+                          "Line" = "line")
+  ),
+  
+  wellPanel(
+    p(strong("Date range (back from present)")),
+    sliderInput(inputId = "time_num",
+                label = "Time Number",
+                min = 1, max = 24, step = 1, value = 6),
+    
+    selectInput(inputId = "time_unit",
+                label = "Time Unit",
+                choices = c("Day" = "day",
+                            "Weeks" = "weeks",
+                            "Months" = "months",
+                            "Years" = "years"),
+                selected = "day")
+  )),
   
   # Main panel (on the right hand side)
   
-  mainPanel(
-    verbatimTextOutput("summary"),
-    
-    tableOutput("table")
-  )
-  
+  mainPanel(plotOutput("plotSymbol"))
   ))
 
   
